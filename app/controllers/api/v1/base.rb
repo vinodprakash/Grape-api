5a4c7fd64ead4a5416eb515bcb0fb29299f73f9b
vinodprakash <psvinodprakash@gmail.com>
2016-01-12 11:40:40 +0530

    Added grape swagger to base class

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -1,7 +1,15 @@
+require "grape-swagger"
 module API  
 	module V1
 		class Base < Grape::API
 			mount API::V1::Graduates
+
+			add_swagger_documentation(
+				api_version:"v1",
+				hide_documentation_path: true,
+				mount_path: "/api/v1/swagger_doc",
+				hide_foramt: true
+			)
 		end
 	end
 end 
