32612038a67bad2a1a7fd43d832ffe22173adcef
vinodprakash <psvinodprakash@gmail.com>
2016-01-12 11:33:56 +0530

    Added Serializer

--- app/api/v1/defaults.rb
+++ app/api/v1/defaults.rb
@@ -9,25 +9,25 @@ module API
         default_format :json
         format :json
         formatter :json, 
-             Grape::Formatter::ActiveModelSerializers
+        Grape::Formatter::ActiveModelSerializers
 
         helpers do
-          def permitted_params
-            @permitted_params ||= declared(params, 
-               include_missing: false)
-          end
+        def permitted_params
+        @permitted_params ||= declared(params, 
+        include_missing: false)
+        end
 
-          def logger
-            Rails.logger
-          end
+        def logger
+        Rails.logger
+        end
         end
 
         rescue_from ActiveRecord::RecordNotFound do |e|
-          error_response(message: e.message, status: 404)
+        error_response(message: e.message, status: 404)
         end
 
         rescue_from ActiveRecord::RecordInvalid do |e|
-          error_response(message: e.message, status: 422)
+        error_response(message: e.message, status: 422)
         end
       end
     end
---
+++ app/serializers/graduate_serializer.rb
@@ -0,0 +1,3 @@
+class GraduateSerializer < ActiveModel::Serializer
+	attributes :id, :first_name, :last_name, :cohort, :current_job, :bio, :news, :website, :picture, :created_at, :updated_at
+end
